3b4f7e30a3add205846f3abf39a9520c660a099c
krishna sinha <nitksinhaji@gmail.com>
2016-09-25 12:53:07 +0530

    model done

---
+++ polls/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^userlogin/', views.userlogin, name='userlogin'),
+
+   # url(r'^(detail.html)$', views.index, name='call'),
+
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
